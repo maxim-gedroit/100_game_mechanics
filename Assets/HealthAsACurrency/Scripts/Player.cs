736f2752dc1edc0f455e814e4ab1d3a3a7995aa0
Максим Гедроить <makkoreslav@gmail.com>
2023-02-17 19:37:10 +0100

    ref

---
+++ Assets/HealthAsACurrency/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace HealthAsACurrency.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
